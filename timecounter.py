ff5777cff6e54f4261e05b3fa8c17d8d70b36ac0
62070021 <54878165+62070021@users.noreply.github.com>
2019-11-13 19:38:00 +0700

    Update timecounter.py

--- timecounter.py
+++ timecounter.py
@@ -4,15 +4,15 @@ import easygui
 last_time = time.time()
 elapse = 0
 while True:
-    if elapse <= 10:
-        elapse = time.time() - last_time
+    if elapse <= 10: #ถ้าเกิน 10 หน่วยเป็นวินาที
+        elapse = time.time() - last_time #ระบุเวลาที่ผ่านไป
         print(elapse)
     else:
         print('Rest your eye!')
-        value = easygui.ynbox('Rest your eye!', 'Face for relax',('yes','no'))
+        value = easygui.ynbox('Rest your eye!', 'Face for relax',('yes','no')) #ตัวเลือก
         if value == False:
             break
         else:
-            time.sleep(5)
+            time.sleep(5) #ระยะเวลาการพัก
         elapse = 0
         last_time = time.time()
