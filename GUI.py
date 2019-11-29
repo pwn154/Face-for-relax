e43ddf956ae66428ed552dcce13a46738e1d7b61
PrathanNawiang62070113 <56547496+PrathanNawiang62070113@users.noreply.github.com>
2019-11-29 15:15:37 +0700

    Update and rename GUI to GUI.py

---
+++ GUI.py
@@ -0,0 +1,19 @@
+from tkinter import *
+##code
+### StarT Gui ###
+gui=Tk()
+gui.geometry('650x500')
+gui.title("Face For Relax")
+gui.configure(background='#BDBDBD')
+mlabel=Label(text="Face For Relax",fg="#FFFF00",bg="black").pack()
+###photo
+button = Button(gui, text='Stop', width=25, command=gui.destroy)
+button.pack() 
+gui.mainloop()
+
+
+
+#สามารถกำหนดเวลาได้ภายในนี้
+#มีปุ่ม Start Stop
+#มีเสียง มี Pop up desktop
+#
