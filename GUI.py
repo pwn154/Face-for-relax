1a8350ab54692ba65af96e0cfac0b836d80fb3d5
62070021 <54878165+62070021@users.noreply.github.com>
2019-12-11 15:59:08 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -18,6 +18,6 @@ def show_frame():
     lmain.after(100, show_frame)
 
 show_frame()
-bottom1 = Button(gui, text='Start', width=25, commnand=#เรียกใช้ฟังชั่นนับ)
+bottom1 = Button(gui, text='Start', width=25, commnand=gui.destroy).pack()
 button2 = Button(gui, text='Stop', width=25, command=gui.destroy).pack()
 gui.mainloop()
