6d4746ee970e2c98afe513cbc9f7533c4badca6d
62070021 <54878165+62070021@users.noreply.github.com>
2019-12-12 13:44:34 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
+from tkinter.messagebox import showinfo
 gui = Tk()
 gui.wm_title('Face For Relax')
 gui.geometry('650x600')#ขนาดGUI
@@ -17,11 +18,21 @@ def show_frame():
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(100, show_frame)
-def popup_bonus():
-    win = Toplevel()
-    win.wm_title("About us")
-    l1 = Label(win, text="Kanasin Aonrkittisarn")
-Button1 = Button(text='Start', command=gui.destroy).pack()#ยังไม่ได้แก้ Command
-Button2 = Button(text='About', command=popup_bonus).pack()#กะจะใส่ฟังก์ชั่นรายชื่อ Command
+def popup_showinfo():
+    showinfo('My Group', 'Kanasin Amoornkittisarn 62070021\nTatchaphon Sriargardkraisang 62070090\nPrathan Nawieng 62070113\nPattarapol Ngaorattanaphanthikun 62070147\nPuwanut Janmee 62070154')
+class Application(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.pack()
+
+        
+        self.button_showinfo = Button(self, text="Start", command=gui.destroy)
+        self.button_showinfo.pack()
+        
+        self.button_showinfo = Button(self, text="Show Info", command=popup_showinfo)
+        self.button_showinfo.pack()
+
+app = Application(gui)
 show_frame()
 gui.mainloop()
