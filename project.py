c903279d65ead775dcc9ab354181a0858b3a8838
pwn154 <56547066+pwn154@users.noreply.github.com>
2019-12-12 15:30:33 +0700

    Add GUI to project (No timer)

--- project.py
+++ project.py
@@ -1,10 +1,21 @@
 import cv2
 import time
 import easygui
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 last_time = time.time()
 elapse = 0
+
+gui = Tk()
+gui.wm_title('Face For Relax')
+gui.geometry('650x600')#ขนาดGUI
+gui.configure(background='#000000')
+mlabel=Label(text="Face For Relax",fg="#FFFF00", bg='#000000').pack()
+lmain = Label(gui)
+lmain.pack()
 
 def draw_boundary(img, classifier, scaleFactor, minNeightbors, color, text): #color(BGR) >> Blue Green Red
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #แปลงภาพสีเป็นภาพขาวดำ
@@ -22,27 +33,49 @@ def detect(img, faceCascade):
 
 cap = cv2.VideoCapture(0) # 0 = internal webcam, -1 = external webcam
 
-while True:
-    ret, frame = cap.read() # อ่านภาพจากกล้องมาทีละ frame, 1 loop = 1 frame
+def show_frame():
+    ret, frame = cap.read()
     frame, coordinate = detect(frame, faceCascade)
-    cv2.imshow('frame', frame) #แสดง frame
-    if (cv2.waitKey(100) & 0xFF == 27): #27 = esc  >>> ปิดหน้าต่าง
-        break
-    if elapse <= 20:
-        if coordinate != []:
-            elapse = time.time() - last_time #ระบุเวลาที่ผ่านไป
-            print(elapse)
-        else:
-            last_time = time.time() - elapse
-    else:
-        print('Rest your eye!')
-        value = easygui.ynbox('Rest your eye!', 'Face for relax',('yes','no')) #ตัวเลือก
-        if value == False:
-            break
-        else:
-            time.sleep(2) #ระยะเวลาการพัก
-        elapse = 0
-        last_time = time.time()
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(100, show_frame)
 
-cap.release()
-cv2.destroyAllWindows()
+def popup_showinfo():
+    messagebox.showinfo(title='Group Member', message=
+'62070021 Kanasin Amoornkittisarn\n\
+62070090 Tatchaphon Sriargardkraisang\n\
+62070113 Prathan Nawieng\n\
+62070147 Pattarapol Ngaorattanaphanthikun\n\
+62070154 Puwanut Janmee')
+
+# while True:
+#     ret, frame = cap.read() # อ่านภาพจากกล้องมาทีละ frame, 1 loop = 1 frame
+#     frame, coordinate = detect(frame, faceCascade)
+#     cv2.imshow('frame', frame) #แสดง frame
+#     if (cv2.waitKey(100) & 0xFF == 27): #27 = esc  >>> ปิดหน้าต่าง
+#         break
+#     if elapse <= 20:
+#         if coordinate != []:
+#             elapse = time.time() - last_time #ระบุเวลาที่ผ่านไป
+#             print(elapse)
+#         else:
+#             last_time = time.time() - elapse
+#     else:
+#         print('Rest your eye!')
+#         value = easygui.ynbox('Rest your eye!', 'Face for relax',('yes','no')) #ตัวเลือก
+#         if value == False:
+#             break
+#         else:
+#             time.sleep(2) #ระยะเวลาการพัก
+#         elapse = 0
+#         last_time = time.time()
+
+show_frame()
+
+Button1 = Button(text='Start', command=gui.destroy).pack()
+Button2 = Button(text='About', command=popup_showinfo).pack()
+
+gui.mainloop()
